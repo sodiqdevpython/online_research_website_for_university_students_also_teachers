10e0bf1827f65497ad14fc240f774920d17d0782
Sodiq <sodiq7898@gmail.com>
2023-10-09 15:57:50 +0500

    deploy for github

---
+++ virt/Scripts/peek.py
@@ -0,0 +1,18 @@
+#!C:\Users\sodiq\OneDrive\Ishchi stol\last_clone\virt\Scripts\python.exe
+import sys
+from jwkest import jwe
+from jwkest import jws
+
+__author__ = 'roland'
+
+jwt = sys.argv[1]
+
+_jw = jwe.factory(jwt)
+if _jw:
+    print("jwe")
+else:
+    _jw = jws.factory(jwt)
+    if _jw:
+        print("jws")
+        print(_jw.jwt.headers)
+        print(_jw.jwt.part[1])
